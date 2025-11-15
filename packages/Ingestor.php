0ce10a86a71144ec3c9e640a6428f848ab4ce037
rakibtg <rakibtg@gmail.com>
2025-11-15 21:43:54 +0100

    Refactor Ingestor class constructor to remove currentVersion parameter and enhance version handling; update getVersions method to initialize current, bug fix only, and archived versions.

--- packages/Ingestor.php
+++ packages/Ingestor.php
@@ -6,11 +6,34 @@ class Ingestor
 {
   private $docsDirectory;
   private $currentVersion;
+  private $bugFixOnlyVersion;
+  private $archivedVersions;
 
-  function __construct($currentVersion)
+  function __construct()
   {
     $this->docsDirectory = realpath(__DIR__ . "/../../docs/");
-    $this->currentVersion = $currentVersion;
+    $this->getVersions();
+  }
+
+  function getVersions()
+  {
+    $versions = $this->getAvailableVersions();
+
+    // Filter only directories
+    $versions = array_filter($versions, function ($item) {
+      return is_dir($this->docsDirectory . '/' . $item);
+    });
+
+    // Sort versions using version_compare in descending order
+    usort($versions, function ($a, $b) {
+      return version_compare($b, $a);
+    });
+
+    $versions = array_values($versions); // Re-index array
+
+    $this->currentVersion = isset($versions[0]) ? $versions[0] : '';
+    $this->bugFixOnlyVersion = isset($versions[1]) ? $versions[1] : '';
+    $this->archivedVersions = array_slice($versions, 2);
   }
 
   function getAvailableVersions()
@@ -24,7 +47,12 @@ class Ingestor
   function ingest()
   {
     foreach ($this->getAvailableVersions() as $key => $version) {
-      (new Compiler($version))->setCurrentVersion($this->currentVersion)->compile();
+      (new Compiler($version))->setCurrentVersion(
+        $this->currentVersion,
+        $this->bugFixOnlyVersion,
+        $this->archivedVersions
+      )
+        ->compile();
     }
   }
 }
